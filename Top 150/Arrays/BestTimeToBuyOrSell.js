6e6f43fdcc99b2a3c98574575d4bd3ee2f6897d3
Sukhbir Singh <Sukhbirbrar83@gmail.com>
2023-10-07 00:59:00 -0700

    Updated the Best Time to buy or sell a stock

--- Top 150/Arrays/BestTimeToBuyOrSell.js
+++ Top 150/Arrays/BestTimeToBuyOrSell.js
@@ -1,18 +1,35 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+*/
+
 var maxProfit = function(prices) {
-    let max = -Infinity;
-    let left = 0;
-    let right = prices.length - 1;
-    let min = Infinity;
-
-    while(left <= right){
-        max = Math.max(max, prices[right]);
-        min = Math.min(min, prices[left]);
-        left++;
-        right--;
+    if(prices == null || prices.length <= 1) return 0; 
+    let min = prices[0];
+    let profit = 0;
+    for(let i = 1; i < prices.length; i++){
+        min = Math.min(min, prices[i]);
+        profit = Math.max(profit, prices[i] - min);
     }
-    console.log(max , min)
 
-    return max - min <= 0 ? 0 : max - min;
+    return profit;
 };
 
-console.log(maxProfit([3,2,6,5,0,3]))
+console.log(maxProfit([7,1,5,3,6,4]))
